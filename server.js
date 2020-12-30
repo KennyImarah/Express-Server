36926b0533bf1cd4699cf0ee9f504cf2886edda3
Kenny I <kenny.o.imarah@gmail.com>
2020-12-30 01:46:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 app.get("/", function(req, res){
   res.send("<h1>Hello world</h1>");
 });
+
+app.get("/contact", function(req, res){
+  res.send("Contact me at: kenny.imarah@willmoon.tech");
+});
+
+app.get("/shop", function(req, res){
+  res.send("Shop at: willmoon.tech");
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
